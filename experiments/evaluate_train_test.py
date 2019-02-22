445aec4fbbb31af3077d7b9bd48a88316b3494dc
SrMouraSilva <mateus.moura@hotmail.com>
2019-02-21 22:14:51 -0300

    Run tests in models with new metrics

--- experiments/evaluate_train_test.py
+++ experiments/evaluate_train_test.py
@@ -7,7 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.random_projection import GaussianRandomProjection
 
-from experiments.model_evaluate.evaluate_method import mrr_score_function
+from experiments.model_evaluate.evaluate_method.evaluate_method import hit_ratio_score_function, mdcg_score_function, mrr_score_function
 from experiments.model_evaluate.model_evaluate import ModelEvaluate
 from experiments.model_evaluate.split_method import split_x_y, \
     split_with_one_hot_encoding_function
@@ -28,10 +28,36 @@ n_labels = 117
 # (model, params like grid search, split data method)
 
 models = [
-    #(LogisticRegression, {}, split_with_one_hot_encoding_function(n_labels)),
-    #(svm.SVC, {'C': [10000.0], 'gamma': [1e-05], 'kernel': ['rbf'], 'probability': [True]}, split_with_one_hot_encoding_function(n_labels)),
+    # k-NN
+    # - Accuracy
     #(KNeighborsClassifier, {'n_neighbors': [15], 'algorithm': ['brute'], 'metric': ['hamming']}, split_x_y),
-    (MLPClassifier, {'hidden_layer_sizes': [80], 'max_iter': [500]}, split_with_one_hot_encoding_function(n_labels)),
+    # - Hit@5
+    #(KNeighborsClassifier, {'n_neighbors': [40], 'algorithm': ['brute'], 'metric': ['hamming']}, split_x_y),
+    # - MRR, NDGC
+    #(KNeighborsClassifier, {'n_neighbors': [60], 'algorithm': ['brute'], 'metric': ['hamming']}, split_x_y),
+    # - MAE
+    # ?
+
+    # LogisticRegression
+    # - Accuracy, Hit@5, MRR, NDGC, MAE
+    #(LogisticRegression, {}, split_with_one_hot_encoding_function(n_labels)),
+    # -
+    # -
+    # -
+    # -
+
+    # SVM
+    # - Accuracy, NDGC
+    (svm.SVC, {'C': [10.0], 'gamma': ['scale'], 'kernel': ['rbf'], 'probability': [True]}, split_with_one_hot_encoding_function(n_labels)),
+    # - Hit@5
+    (svm.SVC, {'C': [1000.0], 'gamma': ['scale'], 'kernel': ['rbf'], 'probability': [True]}, split_with_one_hot_encoding_function(n_labels)),
+    # - MRR
+    (svm.SVC, {'C': [10.0], 'gamma': [0.1], 'kernel': ['rbf'], 'probability': [True]}, split_with_one_hot_encoding_function(n_labels)),
+    # - MAE
+
+    # MLP
+    # - Accuracy
+    #(MLPClassifier, {'hidden_layer_sizes': [80], 'max_iter': [500]}, split_with_one_hot_encoding_function(n_labels)),
 ]
 
 ##############
@@ -41,7 +67,10 @@ path = Path('evaluate_results/train-test')
 
 metrics = {
     'accuracy': 'accuracy',
-    #'mrr': mrr_score_function(n_labels)
+    'hit@5': hit_ratio_score_function(5, n_labels),
+    'mrr': mrr_score_function(n_labels),
+    'mdcg': mdcg_score_function(n_labels),
+    #'map':
 }
 
 
